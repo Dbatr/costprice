eeb02135f73c2fb9c776cb8efa53595d34039b09
Dbatr <bdanis2311@gmail.com>
2024-04-04 17:18:22 +0500

    empty project

---
+++ src/main/java/ru/fusing/costprice/CostpriceApplication.java
@@ -0,0 +1,13 @@
+package ru.fusing.costprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostpriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fusing/costprice/CostpriceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fusing.costprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
