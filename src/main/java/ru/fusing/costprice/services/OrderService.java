ae952d4af5ebfe53f4bbee4349e75733d1a17457
Dbatr <bdanis2311@gmail.com>
2024-04-18 22:54:30 +0500

    few changes

--- src/main/java/ru/fusing/costprice/services/OrderService.java
+++ src/main/java/ru/fusing/costprice/services/OrderService.java
@@ -51,48 +51,19 @@ public class OrderService {
 
         AtomicReference<Double> totalPrice = new AtomicReference<>(material.getPrice());
 
-        // Processing information about components and sizes
-        List<OrderComponent> orderComponents = orderDTO.getComponents().stream().map(componentDTO -> {
-            Component component = componentRepository.findById(componentDTO.getComponentId())
-                    .orElseThrow(() -> new RuntimeException("Component not found"));
-            Size size = sizeRepository.findById(componentDTO.getSizeId())
-                    .orElseThrow(() -> new RuntimeException("Size not found"));
+        List<OrderComponent> orderComponents = getOrderComponents(orderDTO, totalPrice, order);
+        List<OrderInstrument> orderInstruments = getOrderInstruments(orderDTO, totalPrice, order);
+        List<OrderExpenses> orderExpenses = getOrderExpenses(orderDTO, totalPrice, order);
 
-            Double componentPrice = component.getPrice() * size.getPrice();
-            totalPrice.updateAndGet(v -> v * componentPrice);
+        order.setComponents(orderComponents);
+        order.setInstruments(orderInstruments);
+        order.setExpenses(orderExpenses);
+        order.setTotalPrice(totalPrice.get());
+        return orderRepository.save(order);
+    }
 
-            // Вычитание единицы из количества компонента в ComponentStock
-            ComponentStock componentStock = componentStockRepository.findByComponentId(component.getId());
-            if (componentStock == null) {
-                throw new RuntimeException("Component stock not found");
-            }
-            if (componentStock.getQuantity() < 1) {
-                throw new RuntimeException("Not enough components in stock");
-            }
-            componentStock.setQuantity(componentStock.getQuantity() - 1);
-            componentStockRepository.save(componentStock);
-
-            OrderComponent orderComponent = new OrderComponent();
-            orderComponent.setComponent(component);
-            orderComponent.setSize(size);
-            orderComponent.setOrder(order);
-            return orderComponent;
-        }).collect(Collectors.toList());
-
-        // Processing information about instruments
-        List<OrderInstrument> orderInstruments = orderDTO.getInstrumentIds().stream().map(instrumentId -> {
-            Instrument instrument = instrumentRepository.findById(instrumentId)
-                    .orElseThrow(() -> new RuntimeException("Instrument not found"));
-            totalPrice.updateAndGet(v -> v + instrument.getPrice());
-
-            OrderInstrument orderInstrument = new OrderInstrument();
-            orderInstrument.setInstrument(instrument);
-            orderInstrument.setOrder(order);
-            return orderInstrument;
-        }).collect(Collectors.toList());
-
-        // Processing information about expenses
-        List<OrderExpenses> orderExpenses = orderDTO.getExpensesIds().stream().map(expensesId -> {
+    private List<OrderExpenses> getOrderExpenses(OrderDTO orderDTO, AtomicReference<Double> totalPrice, Order order) {
+        return orderDTO.getExpensesIds().stream().map(expensesId -> {
             Expenses expenses = expensesRepository.findById(expensesId)
                     .orElseThrow(() -> new RuntimeException("Expenses not found"));
             totalPrice.updateAndGet(v -> v + expenses.getPrice());
@@ -102,12 +73,51 @@ public class OrderService {
             orderExpense.setOrder(order);
             return orderExpense;
         }).collect(Collectors.toList());
+    }
 
-        order.setComponents(orderComponents);
-        order.setInstruments(orderInstruments);
-        order.setExpenses(orderExpenses);
-        order.setTotalPrice(totalPrice.get());
-        return orderRepository.save(order);
+    private List<OrderInstrument> getOrderInstruments(OrderDTO orderDTO, AtomicReference<Double> totalPrice, Order order) {
+        return orderDTO.getInstrumentIds().stream().map(instrumentId -> {
+            Instrument instrument = instrumentRepository.findById(instrumentId)
+                    .orElseThrow(() -> new RuntimeException("Instrument not found"));
+            totalPrice.updateAndGet(v -> v + instrument.getPrice());
+
+            OrderInstrument orderInstrument = new OrderInstrument();
+            orderInstrument.setInstrument(instrument);
+            orderInstrument.setOrder(order);
+            return orderInstrument;
+        }).collect(Collectors.toList());
+    }
+
+    private List<OrderComponent> getOrderComponents(OrderDTO orderDTO, AtomicReference<Double> totalPrice, Order order) {
+        return orderDTO.getComponents().stream().map(componentDTO -> {
+            Component component = componentRepository.findById(componentDTO.getComponentId())
+                    .orElseThrow(() -> new RuntimeException("Component not found"));
+            Size size = sizeRepository.findById(componentDTO.getSizeId())
+                    .orElseThrow(() -> new RuntimeException("Size not found"));
+
+            Double componentPrice = component.getPrice() * size.getPrice();
+            totalPrice.updateAndGet(v -> v * componentPrice);
+
+            updateComponentStock(component);
+
+            OrderComponent orderComponent = new OrderComponent();
+            orderComponent.setComponent(component);
+            orderComponent.setSize(size);
+            orderComponent.setOrder(order);
+            return orderComponent;
+        }).collect(Collectors.toList());
+    }
+
+    private void updateComponentStock(Component component) {
+        ComponentStock componentStock = componentStockRepository.findByComponentId(component.getId());
+        if (componentStock == null) {
+            throw new RuntimeException("Component stock not found");
+        }
+        if (componentStock.getQuantity() < 1) {
+            throw new RuntimeException("Not enough components in stock");
+        }
+        componentStock.setQuantity(componentStock.getQuantity() - 1);
+        componentStockRepository.save(componentStock);
     }
 
     public List<Order> findAllOrders() {
